64c8a82266390e7d39dcaf0390a9e1e1fb7da0c5
khan47138 <63116305+khan47138@users.noreply.github.com>
2020-04-03 09:35:21 -0700

    Add files via upload

---
+++ khan.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('C:\\Users\\Hp-Desktop\\Desktop\\Web App\\web.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    #For rendering results on HTML GUI
+    
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    if prediction==0:
+          return render_template('index.html', prediction_text='You have low Diabetes')
+    elif prediction==1:
+        return render_template('index.html',prediction_text="You have high Diabetes")
+    else:
+        return render_template('index.html',prediction_text='You have middle level of diabete' )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
